47abd5dd19eb1da57298614107f2c114bda5290c
KrLite <68179735+KrLite@users.noreply.github.com>
2023-03-31 20:21:28 +0800

    2.0.0

--- src/main/java/net/krlite/stenographer/Stenographer.java
+++ src/main/java/net/krlite/stenographer/Stenographer.java
@@ -5,9 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Stenographer implements ModInitializer {
-	public static final String MOD_NAME = "Stenographer";
-	public static final String MOD_ID = "stenographer";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String NAME = "Stenographer", ID = "stenographer";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/krlite/stenographer/mixin/LanguageSetter.java
+++ src/main/java/net/krlite/stenographer/mixin/LanguageSetter.java
@@ -19,14 +19,23 @@ import java.util.Objects;
 public abstract class LanguageSetter {
 	@Mutable @Shadow public String language;
 
-	@Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;load()V", shift = At.Shift.AFTER))
+	@Inject(
+			method = "<init>",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/option/GameOptions;load()V",
+					shift = At.Shift.AFTER
+			)
+	)
 	private void setLanguage(MinecraftClient client, File optionsFile, CallbackInfo ci) {
 		String language = (Locale.getDefault().getLanguage() + "_" + Locale.getDefault().getCountry()).toLowerCase();
+
 		if (this.language.equals("en_us") && !this.language.equals(language)) { // Only set language if it's English or another language differing from current
 			/*
-			Fix language in case it's not a valid language code(due to some launchers like prism launcher),
-			but only works with certain countries
-			Language codes are from Locale.class
+			Fix language in case it's not a valid language
+			code(due to some launchers like Prism Launcher),
+			but only works with certain countries.
+			Language codes are from Locale.class.
 			 */
 			Locale[] locales = Arrays.stream(Locale.class.getFields()).filter(f -> f.getType().equals(Locale.class)).map(f -> {
 				try {
@@ -36,14 +45,17 @@ public abstract class LanguageSetter {
 					return null;
 				}
 			}).filter(Objects::nonNull).filter(l -> !l.getLanguage().isEmpty() && !l.getCountry().isEmpty()).toArray(Locale[]::new); // Get preset locales from java
+
 			String[] languageCodes = Arrays.stream(locales).map(l -> (l.getLanguage() + "_" + l.getCountry()).toLowerCase()).distinct().toArray(String[]::new); // Get preset language codes from locales
 			String fixedFrom = language;
+
 			if (Arrays.stream(languageCodes).anyMatch(l -> l.split("_")[1].equals(Locale.getDefault().getCountry().toLowerCase()))) {
 				language = Arrays.stream(languageCodes).filter(l -> l.split("_")[1].equals(Locale.getDefault().getCountry().toLowerCase())).findFirst().orElse("en_us");
 			}
-			/* The language should be fixed */
 
-			Stenographer.LOGGER.info("[" + Stenographer.MOD_NAME + "] Switching language to " + language + (!fixedFrom.equals(language) ? ", fixed from " + fixedFrom + "." /* Log the unfixed language */ : "..."));
+			/* Language should be fixed */
+
+			Stenographer.LOGGER.info("[" + Stenographer.NAME + "] Switching language to " + language + (!fixedFrom.equals(language) ? ", fixed from " + fixedFrom + "." /* Log the unfixed language */ : "..."));
 			this.language = language;
 		}
 	}
